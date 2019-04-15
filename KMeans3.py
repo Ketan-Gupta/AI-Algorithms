42451454fcaf1965f928dfdf86f28ffd40100c6b
Ketan <ketan.gupta@protonmail.com>
2019-04-15 21:34:11 +0530

    MatplotUpdate

--- KMeans3.py
+++ KMeans3.py
@@ -2,6 +2,7 @@ import math
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+
 def summation(container, value):
     total = 0
     for x in range(len(container)):
@@ -12,20 +13,23 @@ def euclidean_distance(first,second):
     # first and second =[1,2] typed list
     return math.sqrt((pow((first[0]-second[0]),2))+(pow((first[1]-second[1]),2)))
 
-def knn(parentSet,median1,median2):
-    prev_median1,prev_median2=median1,median2
+def knn(parentSet,median1,median2,median3):
+    prev_median1,prev_median2,prev_median3=median1,median2,median3
     set1 = list()
-    set2=list()
+    set2 = list()
+    set3 = list()
 
     for x in parentSet:
-        if(euclidean_distance(median1,x)<euclidean_distance(median2,x)):
+        if(euclidean_distance(median1,x)<euclidean_distance(median2,x) and euclidean_distance(median1,x)<euclidean_distance(median3,x)):
             set1.append(x)
-        else:
+        elif(euclidean_distance(median2,x)<euclidean_distance(median3,x)):
             set2.append(x)
+        else:
+            set3.append(x)
 
     new_median1 = summation(set1,0)/len(set1),summation(set1,1)/len(set1)
     new_median2 = summation(set2,0)/len(set2),summation(set2,1)/len(set2)
-
+    new_median3 = summation(set3,0)/len(set3),summation(set3,1)/len(set3)
 
 
     while(prev_median1 != new_median1 and prev_median2 != new_median2):
@@ -39,26 +43,30 @@ def knn(parentSet,median1,median2):
                 set2.append(x)
         new_median1 = summation(set1,0)/len(set1),summation(set1,1)/len(set1)
         new_median2 = summation(set2,0)/len(set2),summation(set2,1)/len(set2)
-    
+        new_median3 = summation(set3,0)/len(set3),summation(set3,1)/len(set3)
     print("Set1: Median=",new_median1)
     print(set1)
     print("Set2: Median=",new_median2)
     print(set2)
-    return (set1,set2)
+    print("Set3: Median=",new_median3)
+    print(set3)
+    return (set1,set2,set3)
 
 def driver():
     data = pd.read_csv("KMeans.csv")
     parentSet = list()
     for x in range(len(data)):
         parentSet.append([data.iloc[x][0],data.iloc[x][1]])
-
     #parentSet = [[0.1,0.6],[0.15,0.71],[0.08,0.9],[0.16,0.85],[0.2,0.3],[0.25,0.5],[0.24,0.1],[0.3,0.2]]
-    median1, median2 = parentSet[0],parentSet[7]
-    set1,set2 = knn(parentSet, median1,median2)
+    median1, median2, median3 = parentSet[0],parentSet[7],parentSet[3]
+    set1,set2,set3 = knn(parentSet, median1,median2,median3)
     plt_set1 = np.array(set1)
     plt_set2 = np.array(set2)
+    plt_set3 = np.array(set3)
     plt.scatter(plt_set1[:,0],plt_set1[:,1])
     plt.scatter(plt_set2[:,0],plt_set2[:,1])
+    plt.scatter(plt_set3[:,0],plt_set3[:,1])
     plt.show()
     exit()
+
 driver()
--- Linear.py
+++ Linear.py
@@ -1,5 +1,10 @@
 import math
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# b0 is slope c
+# b1 is y intercept
 
 def estimate_coeff(x,y):
     n = np.size(x)
@@ -9,17 +14,31 @@ def estimate_coeff(x,y):
     SSxx = np.sum(x*x) - n*mean_x*mean_x
     
     b1 = SSxy/SSxx
-    b2 = mean_y - b1*mean_x
-    return (b1,b2)
+    b0 = mean_y - b1*mean_x
+    return (b0,b1)
+
+def plot_regression_line(x,y,b0,b1):
+    plt.scatter(x,y,color="m",marker="o",s=30)
+    y_pred = b0 + x*b1
+    plt.plot(x,y_pred,color="g")
+
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
+    exit()
 
 def driver():
-    x = np.array([10,9,2,15,10,16,11,16])
-    y = np.array([95,80,10,50,45,98,38,93])
-    
-    b1,b2=estimate_coeff(x,y)
-    print("b1 : {} b2 : {}".format(b1,b2))
-    test_y = float(input("Enter the value of Y\n"))
-    print("Predicted X :", b1*test_y+b2)
+    data = pd.read_csv("Linear.csv")
+    x = np.array(data.iloc[:,0])
+    y = np.array(data.iloc[:,-1])
+    #x = np.array([10,9,2,15,10,16,11,16])
+    #y = np.array([95,80,10,50,45,98,38,93])
+    b0,b1=estimate_coeff(x,y)
+    print("b0 : {} b1 : {}".format(b0,b1))
+    test_x = float(input("Enter the value of X\n"))
+    print("Predicted Y :",b0 + b1*test_x) 
+    plot_regression_line(x,y,b0,b1)
+
 
 
 driver()
